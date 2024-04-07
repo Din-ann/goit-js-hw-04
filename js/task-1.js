a7c78d8726a9fe881777eae5d8fc9287cf7dcf1b
Diana-f-5216 <diana.filzhak@gmail.com>
2024-04-07 19:15:25 +0100

    hw-4.corrected

--- js/task-1.js
+++ js/task-1.js
@@ -5,11 +5,7 @@ function isEnoughCapacity(products, containerSize) {
     total += products[key];
   }
 
-  if (total <= containerSize) {
-    return "true";
-  } else {
-    return "false";
-  }
+  return total <= containerSize;
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
